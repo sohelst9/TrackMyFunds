3e5739f60816cdf605491299ac56d36ce830878d
sohelst9 <websohel89@gmail.com>
2024-10-31 02:55:56 +0600

    login, logout api crate and all funtion add

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Unique;
@@ -46,5 +47,64 @@ class AuthController extends Controller
                 'user' => $user
             ]);
         }
+    }
+
+    //--login 
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ]);
+        } else {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
+                $token = $user->createToken('token')->plainTextToken;
+                return response()->json([
+                    'status' => 200,
+                    'success' => "Login Successfully",
+                    'user' => $user,
+                    'token' => $token
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 401,
+                    'error' => 'Invalid credentials'
+                ]);
+            }
+        }
+    }
+
+    //--user
+    public function user()
+    {
+        $user = Auth::user();
+        if ($user) {
+            return response()->json([
+                'status' => 200,
+                'user' => $user
+            ]);
+        } else {
+            return response()->json([
+                'status' => 401,
+                'message' => "user not found"
+            ]);
+        }
+    }
+
+    //-- logout
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => "Logout Successfully"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
