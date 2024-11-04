2ff1692aaa00ec10a95ef894e87b1a52006a7992
sohelst9 <websohel89@gmail.com>
2024-11-04 13:20:59 +0600

    setup sidebar and fontawesome icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "vue-toastification/dist/index.css";
 
+//---font awesome import ---
+import {library} from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 import App from "./components/App.vue";
 import router from "./router";
 import axios from "axios";
@@ -14,7 +19,11 @@ import { useAuth } from "./auth/auth";
 axios.defaults.baseURL = "/api";
 axios.defaults.withCredentials = true;
 
+library.add(fas);
+
 const app = createApp(App);
+
+app.component('fontawesome-icon', FontAwesomeIcon)
 // Verify the token during app loading
 const { checkAuth } = useAuth();
 checkAuth();
--- resources/js/components/pages/Dashboard/app/Sidebar.vue
+++ resources/js/components/pages/Dashboard/app/Sidebar.vue
@@ -2,10 +2,16 @@
     <!-- Sidebar -->
     <nav class="sidebar">
         <h3 class="text-center mb-4">Game Dashboard</h3>
-        <a href="{{ route('dashboard') }}"><i class="fas fa-home"></i> Home</a>
-        <a href="{{ route('games') }}"><i class="fas fa-gamepad"></i> Games</a>
-        <a href="#"><i class="fas fa-chart-line"></i> Analytics</a>
-        <a href="#"><i class="fas fa-users"></i> Users</a>
-        <a href="#"><i class="fas fa-cog"></i> Settings</a>
+        <router-link :to="{ name: 'home' }" class="item"><fontawesome-icon class="icon" icon="home" /> Home </router-link>
+        <router-link :to="{ name: 'home' }" class="item"><fontawesome-icon class="icon" icon="gamepad" /> Games
+        </router-link>
+        <router-link :to="{ name: 'home' }" class="item"><fontawesome-icon class="icon" icon="chart-line" /> Analytics
+        </router-link>
+        <router-link :to="{ name: 'home' }" class="item"><fontawesome-icon class="icon" icon="home" /> Sales </router-link>
+        <router-link :to="{ name: 'home' }" class="item"><fontawesome-icon class="icon" icon="users" /> Users
+        </router-link>
+        <router-link :to="{ name: 'home' }" class="item"><fontawesome-icon class="icon" icon="fa-cog" /> Settings
+        </router-link>
+
     </nav>
 </template>
